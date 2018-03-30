9c782fac6ef1ab65d0fafa32ae6b34e1d7859c38
Thomas Fromont <tomstom-du-01@hotmail.fr>
2018-03-31 00:35:43 +0200

    search vk lib only for windows system

--- Libraries/findVulkan.lua
+++ Libraries/findVulkan.lua
@@ -1,11 +1,15 @@
 -- findVulkan.lua
 function includeVulkan()
-	-- LunarG's Vulkan SDK set a Windows environment variable, so use it
-	local envVulkanSDKPath = os.getenv("VK_SDK_PATH")
+	filter "system:Windows"
+		-- LunarG's Vulkan SDK set a Windows environment variable, so use it
+		local envVulkanSDKPath = os.getenv("VK_SDK_PATH")
 
-	if (envVulkanSDKPath) then
-		local vulkanSDKPath = envVulkanSDKPath:gsub("\\", "/")
-		print("Found Vulkan librarie : " .. vulkanSDKPath)
-		includedirs { vulkanSDKPath .. "/Include" }
-	end
+		if (envVulkanSDKPath) then
+			local vulkanSDKPath = envVulkanSDKPath:gsub("\\", "/")
+			print("Found Vulkan librarie : " .. vulkanSDKPath)
+			includedirs { vulkanSDKPath .. "/Include" }
+		else
+			print("Vulkan SDK not found !")
+		end
+	filter {}
 end
