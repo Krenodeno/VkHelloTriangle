625715a17356bf1f46a9bf17687f054a62c290c4
Thomas Fromont <tomstom-du-01@hotmail.fr>
2018-04-10 00:53:27 +0200

    Fixed Vulkan include

--- Libraries/findVulkan.lua
+++ Libraries/findVulkan.lua
@@ -2,27 +2,12 @@
 function includeVulkan()
 	-- LunarG's Vulkan SDK set an environment variable, so use it
 	local envVulkanSDKPath = os.getenv("VULKAN_SDK")
-	
-	filter "system:Windows"
-		if (envVulkanSDKPath) then
-			-- get rid of the counter slash
-			local vulkanSDKPath = envVulkanSDKPath:gsub("\\", "/")
-			print("Found Vulkan library : " .. vulkanSDKPath)
-			includedirs { vulkanSDKPath .. "/Include" }
-		else
-			print("Vulkan SDK not found !")
-		end
-		
-	filter "system:Linux"
-		-- try to find the lib before searching for the LunarG SDK
-		if (p = os.findlib("vulkan")) then
-			print("Found Vulkan library : " .. p)
-		elseif (envVulkanSDKPath) then
-			print(("Found Vulkan library : " .. envVulkanSDKPath)
-			includedirs { envVulkanSDKPath .. "/Include" }
-		else
-			print("Vulkan SDK not found !")
-		end
-	
-	filter {}
+	if (envVulkanSDKPath) then
+		-- get rid of the counter slash of Windows (does nothing on Linux)
+		includedirs { envVulkanSDKPath:gsub("\\", "/") .. "Include" }
+	elseif (os.findlib("vulkan")) then
+		-- use the well known include paths
+	else
+		print ("Vulkan SDK not found !")
+	end
 end
