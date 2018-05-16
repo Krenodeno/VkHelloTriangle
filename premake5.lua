3b8dfd6496add801ce64a213a98f311303aad59c
Thomas Fromont <tomstom-du-01@hotmail.fr>
2018-05-16 16:21:59 +0200

    Fixed Vulkan Include and added Linux Vulkan SDK support

--- premake5.lua
+++ premake5.lua
@@ -45,11 +45,12 @@ workspace "VkHelloTriangle"
 
 	include( "Libraries/findVulkan.lua" )
 
+	includeVulkan()
+
 project "VkHelloTriangle"
 	kind "StaticLib"
 
 	includedirs { LibDir .. "Include" }
-	includeVulkan()
 
 	files { sourceDir .. "**.hpp", sourceDir .. "**.inl", sourceDir .. "**.cpp" }
 
@@ -57,7 +58,6 @@ project "VkHelloTriangle"
 project "printVulkanInfos"
 	kind "ConsoleApp"
 	includedirs { LibDir .. "Include", sourceDir }
-	includeVulkan()
 	files { "examples/printCapabilities.cpp" }
 
 	links "VkHelloTriangle"
@@ -65,7 +65,6 @@ project "printVulkanInfos"
 project "vulkan-tutorial"
 	kind "ConsoleApp"
 	includedirs { LibDir .. "Include", sourceDir  }
-	includeVulkan()
 	files { "examples/vulkan-tutorial/*", "ressources/shaders/*.frag", "ressources/shaders/*.vert" }
 
 	filter "system:Windows"
