f7b472456dfa868911a761e7993bc4c578222552
MacGamerFR <tomstom-du-01@hotmail.fr>
2018-09-04 19:41:53 +0200

    update findVulkan.lua from vulkanhpp branch

--- Libraries/findVulkan.lua
+++ Libraries/findVulkan.lua
@@ -4,13 +4,37 @@ function includeVulkan()
 	local envVulkanSDKPath = os.getenv("VULKAN_SDK")
 	if (envVulkanSDKPath) then
 		filter "system:Windows"
-			-- get rid of the counter slash of Windows (does nothing on Linux)
+			-- get rid of the counter slash of Windows
 			includedirs { envVulkanSDKPath:gsub("\\", "/") .. "/Include" }
 		filter "system:Linux"
 			includedirs { envVulkanSDKPath .. "/include" }
 		filter {}
 	elseif (os.findlib("vulkan")) then
 		-- use the well known include paths
+	else
+		print ("Vulkan SDK not found !")
+	end
+end
+
+function linkVulkan()
+-- LunarG's Vulkan SDK set an environment variable, so use it
+	local envVulkanSDKPath = os.getenv("VULKAN_SDK")
+	if (envVulkanSDKPath) then
+		filter "system:Windows"
+			-- get rid of the counter slash of Windows
+			libdirs { envVulkanSDKPath:gsub("\\", "/") .. "/Lib" }
+			links { "vulkan-1" }
+		filter "system:Linux"
+			libdirs { envVulkanSDKPath .. "/lib" }
+			links { "vulkan" }
+		filter {}
+	elseif (os.findlib("vulkan")) then
+		-- use the well known include paths
+		filter "system:Windows"
+			links { "vulkan-1" }
+		filter "system:Linux"
+			links { "vulkan" }
+		filter {}
 	else
 		print ("Vulkan SDK not found !")
 	end
